0027903e4a83657a5108388365633a0835bdd4b5
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-26 11:37:23 -0800

    Reroute server and database endpoints to Postgres

--- Postgres_DB/model.js
+++ Postgres_DB/model.js
@@ -11,13 +11,37 @@ const knex = Knex({
   },
 });
 
-const start = new Date();
-knex.raw('select * from "shoes" where id=120000000')
-  .catch((err) => {
-    console.log(err);
-  })
-  .then((res) => {
-    const end = new Date();
-    console.log(res.rows[0]);
-    console.log(`Took ${end - start} milliseconds to query the database for a single record ...`);
-  });
+// const start = new Date();
+// knex.raw('select * from "shoes" where id=120000000')
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .then((res) => {
+//     const end = new Date();
+//     console.log(res.rows[0]);
+//     console.log(`Took ${end - start} milliseconds to query the database for a single record ...`);
+//   });
+
+class Shoe {
+  // eslint-disable-next-line max-len
+  static addOne(prodSku, priceFull, priceSale, prodCat, prodCols, prodLine, revsAvg, revsCnt, imgSrc) {
+    knex.raw('INSERT INTO shoes (product_sku, price_full, price_sale, product_cat, product_colors, product_line, reviews_avg, reviews_cnt, image_source) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [prodSku, priceFull, priceSale, prodCat, prodCols, prodLine, revsAvg, revsCnt, imgSrc])
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static getOne(id, cb) {
+    knex.raw('SELECT * FROM shoes WHERE id = ? LIMIT 1', [id])
+      .then((res) => { cb(null, res); })
+      .catch((err) => { cb(err, null); });
+  }
+
+  static getImagesOfTwelveSimilar(category, cb) {
+    knex.raw('SELECT * FROM shoes WHERE product_cat = ? LIMIT 12', [category])
+      .then((res) => { cb(null, res); })
+      .catch((err) => { cb(err, null); });
+  }
+}
+
+module.exports = Shoe;
--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const parser = require('body-parser');
 const morgan = require('morgan');
 
-const model = require('./../database/model.js');
+// const model = require('./../database/model.js');
+const Shoe = require('./../Postgres_DB/model.js');
 
 const app = express();
 
@@ -13,47 +14,64 @@ app.use(express.static(path.join(__dirname, './../node_modules')));
 app.use(parser.json());
 app.use(morgan('dev'));
 
+// app.get('/:product_sku/similar', (req, res) => {
+//   model.Shoe.getOne(req.params.product_sku, (err1, data1) => {
+//     if (err1) {
+//       res.status(500).send(err1.message);
+//     } else {
+//       const opts = [data1[0].product_line, data1[0].product_cat, data1[0].product_sku];
+//       model.Shoe.getImagesOfTwelveSimilar(opts, (err2, data2) => {
+//         if (err2) {
+//           res.status(500).send(err2.message);
+//         } else {
+//           res.send(data2);
+//         }
+//       });
+//     }
+//   });
+// });
+
 app.get('/:product_sku/similar', (req, res) => {
-  model.Shoe.getOne(req.params.product_sku, (err1, data1) => {
+  const randID = Math.floor(Math.random() * 120000000);
+  Shoe.getOne(randID, (err1, data1) => {
     if (err1) {
-      res.status(500).send(err1.message);
+      res.status(500).send(err1);
     } else {
-      const opts = [data1[0].product_line, data1[0].product_cat, data1[0].product_sku];
-      model.Shoe.getImagesOfTwelveSimilar(opts, (err2, data2) => {
+      Shoe.getImagesOfTwelveSimilar(data1.rows[0].product_cat, (err2, res2) => {
         if (err2) {
-          res.status(500).send(err2.message);
+          res.status(500).send(err2);
         } else {
-          res.send(data2);
+          res.send(res2.rows);
         }
       });
     }
   });
 });
 
-app.post('/:product_sku/similar', (req, res) => {
-  const info = req.body;
-  model.Shoe.addOne(info.product_sku, info.price_full, info.price_scale, info.product_cat, info.product_colors, info.product_line, 'Best shoe ever', info.reviews_avg, info.reviews_cnt, info.img_src);
-  res.end();
-});
+// app.post('/:product_sku/similar', (req, res) => {
+//   const info = req.body;
+//   model.Shoe.addOne(info.product_sku, info.price_full, info.price_scale, info.product_cat, info.product_colors, info.product_line, 'Best shoe ever', info.reviews_avg, info.reviews_cnt, info.img_src);
+//   res.end();
+// });
 
-app.patch('/:product_sku/similar', (req, res) => {
-  const info = req.body;
-  model.Shoe.updateOne(info.product_sku, info.col, info.val, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    res.end();
-  });
-});
+// app.patch('/:product_sku/similar', (req, res) => {
+//   const info = req.body;
+//   model.Shoe.updateOne(info.product_sku, info.col, info.val, (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     res.end();
+//   });
+// });
 
-app.delete('/:product_sku/similar', (req, res) => {
-  const info = req.body;
-  model.Shoe.deleteOne(info.product_sku, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    res.end();
-  });
-});
+// app.delete('/:product_sku/similar', (req, res) => {
+//   const info = req.body;
+//   model.Shoe.deleteOne(info.product_sku, (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     res.end();
+//   });
+// });
 
 module.exports = app;
