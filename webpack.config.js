712168b39477f3b375b7921883a3d12438b712b8
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-27 11:14:51 -0800

    Update webpack version to allow for compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   context: `${__dirname}/client`,
   entry: './index.jsx',
   output: {
     path: path.resolve(__dirname, './public'),
-    filename: 'bundle.js',
+    filename: 'spc_bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new CompressionPlugin(),
+  ],
   module: {
     rules: [
       {
@@ -23,9 +27,9 @@ module.exports = {
       {
         test: [/\.css$/],
         exclude: [/node_modules/, /coverage/],
-        use: ['style-loader','css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
-    ],  
+    ],
   },
   externals: {
     'react/addons': true, // important!!
